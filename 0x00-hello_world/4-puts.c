35dae95cfa3eb309288a10ea6007e3520c43d390
Petlzzz <omololu369@gmail.com>
2022-12-08 06:57:09 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
 *main - Entry point
 *Return: Always 0 (Successs)
@@ -7,5 +8,7 @@
 int main(void)
 {
    puts("\"Programming is like building a multilingual puzzle");
-           return (0); 
+   return (0);
+
+
 }  	   
