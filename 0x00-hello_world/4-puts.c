94b5b0150dbbea3e840999088a70dc758188eb5a
Petlzzz <omololu369@gmail.com>
2022-12-10 12:02:26 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
-
 /**
+ * main - Entry point, prints Programming is like building
  *
- *  * main - Entry point, prints Programming is like building
- *
- *   *
- *
- *    * Return:Always 0 (success)
- *
- *     */
-
+ * Return:Always 0 (success)
+ */
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
